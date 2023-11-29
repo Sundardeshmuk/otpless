fb8390bc7cc7886f197a13247b95e12f9bb1b88a
sundar sanjay deshmukh <sundardeshmuk01@gmail.com>
2023-11-30 02:03:46 +0530

    first

--- src/App.js
+++ src/App.js
@@ -2,22 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    window.otpless = (otplessUser) => {
+     alert(JSON.stringify(otplessUser));
+    };
+   }, []);
+              
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="otpless-login-page">
+<script type="text/javascript" src="https://otpless.com/auth.js"></script>
+    </div>
     </div>
   );
 }
