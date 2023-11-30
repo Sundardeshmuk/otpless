45f167a6b8d805b5878ab58ded29ae00032091fe
sundar sanjay deshmukh <sundardeshmuk01@gmail.com>
2023-11-30 10:04:21 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from './SIGNUP/Signup';
 import { useEffect } from 'react';
 function App() {
-  <BrowserRouter>
-      <Routes>
-          <Route path="/" element={<Signup />} />
-        
-      </Routes>
+  return (
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<Signup/>}/>
+
+    </Routes>
     </BrowserRouter>
+  );
 }
 
 export default App;
--- src/SIGNUP/Signup.js
+++ src/SIGNUP/Signup.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { useEffect } from 'react';
 function Signup() {
     useEffect(() => {
         window.otpless = (otplessUser) => {
@@ -10,8 +10,9 @@ function Signup() {
       return (
         <div className="App">
         <div id="otpless-login-page">
-    <script type="text/javascript" src="https://otpless.com/auth.js"></script>
+            <h1>asdfef...</h1>
         </div>
+    <script type="text/javascript" src="https://otpless.com/auth.js"></script>
         </div>)
 }
 
